5afdc7a63f29365e19969cf444064c174785dec1
leni <leni.allard@ensta-bretagne.org>
2023-02-13 11:22:58 +0100

    First version of the Faster R-CNN config file with the SSDD dataset

---
+++ configs/faster_rcnn/faster_rcnn_r50_fpn_ssdd.py
@@ -0,0 +1,31 @@
+_base_ = [
+    '../_base_/models/faster_rcnn_r50_fpn.py',  # base Faster R-CNN model
+    '../_base_/datasets/ssdd_dataset.py',     # get configuration of the SSDD dataset
+    '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
+]
+
+optimizer = dict(lr=0.001)
+
+model = dict(
+    roi_head=dict(
+        type='StandardRoIHead',
+        bbox_roi_extractor=dict(
+            type='SingleRoIExtractor',
+            roi_layer=dict(type='RoIAlign', output_size=7, sampling_ratio=0),
+            out_channels=256,
+            featmap_strides=[4, 8, 16, 32]),
+        bbox_head=dict(
+            type='Shared2FCBBoxHead',
+            in_channels=256,
+            fc_out_channels=1024,
+            roi_feat_size=7,
+            num_classes=1,
+            bbox_coder=dict(
+                type='DeltaXYWHBBoxCoder',
+                target_means=[0., 0., 0., 0.],
+                target_stds=[0.1, 0.1, 0.2, 0.2]),
+            reg_class_agnostic=False,
+            loss_cls=dict(
+                type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0),
+            loss_bbox=dict(type='L1Loss', loss_weight=1.0))),
+)
